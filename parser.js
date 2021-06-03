fc2e17e18f5f7ba1694c3dd8ae0a2712fc768d52
Artic67 <sirartic67@gmail.com>
2021-06-04 01:08:47 +0300

    Maked working parser

--- parser.js
+++ parser.js
@@ -2,23 +2,54 @@ const fs = require('fs');
 
 let fileContent = fs.readFileSync("test.json", "utf8");
 
+const inputCheck = (input) => {
+    let start = input.slice(0, 1);
+    if (start === '{') return objectParser(input);
+    else if (start === '[') return arrayParser(input);
+    else if (start === '"') return stringParser(input);
+    else if (start === ':') return [null, input.slice(1)];
+    else if (start === ',') return [null, input.slice(1)];
+}
+
+const spaceParser = (input) => (input.match(/^[\s\n]/)) ? input.slice(input.match(/\S/).index) : input;
+
+const stringParser = (input) => {
+    if (!input.startsWith('"')) return null;
+    let string = "";
+    input = input.slice(1);
+
+    while (input.length > 0 && !input.startsWith('"')) {
+        string += input.slice(0,1);
+        input = input.slice(1);
+    }
+    return (input.startsWith('"')) ? [string, input.slice(1)] : null;
+}
+
 
 const objectParser = (input) => {
     if (!input.startsWith('{')) return null;
-    let object = {}
-    input = input.slice(1, input.length - 1).replace(/\s/g, ''); // replace spaces with ''
-    while (true) {
-        input = stringParser(input);
-        if (!input) return null;
-        let [key, value] = input;
-        if (!value) return null;
-        value = valueParser(value);
-        if (!value) return null;
-        object[key] = value[0];
+    let object = {};
+    input = input.slice(1);
+    let key = "";
+    let value = "";
 
-        if (!temp) break;
-        input = temp[1];
+    while (input.length > 0 && !input.startsWith('}')) {
+        input = spaceParser(input);
+        let returned = inputCheck(input);
+
+        if (typeof(returned[0]) == 'string' && key === ""){
+            key = returned[0];
+        } else if (returned[0] != null && key !== "") {
+            value = returned[0];
+            object[key] = value;
+            key = '';
+            value = '';
+        }
+
+        input = spaceParser(returned[1]);
+
     }
+
     return (input.startsWith('}')) ? [object, input.slice(1)] : null;
 }
 
@@ -26,28 +57,26 @@ const arrayParser = (input) => {
     if (!input.startsWith('[')) return null;
     input = input.slice(1);
     let arr = [];
-    while (true) {
-        let space;
-        input = valueParser(input);
-        if (!input) { return null } arr.push(input[0]);
-        if (!temp) { break } input = temp[1];
+    let el = '';
+
+    while (input.length > 0 && !input.startsWith(']')) {
+        input = spaceParser(input);
+        let returned = inputCheck(input);
+
+        if (returned[0] != null) {
+            el = returned[0];
+            arr.push(el);
+        }
+
+        input = spaceParser(returned[1]);
+
     }
     return (input.startsWith(']')) ? [arr, input.slice(1)] : null;
 }
 
 
-
-const stringParser = (input, i = 1) => {
-    if (!input.startsWith('"')) return null;
-    while (input[i] !== '"') i++;
-    return [input.substring(1, i), input.slice(i + 1)];
-}
-
-
-
-const anyOneParserFactory = (...parsers) => (input) => parsers.reduce((accum, parser) => (accum === null) ? parser(input) : accum);
-const valueParser = anyOneParserFactory(stringParser, arrayParser, objectParser);
 const inpStr = fileContent.toString();
-const output = valueParser(inpStr);
-console.log(output);
-output ? console.log(JSON.stringify(output[0], null, 2)) : console.log('Invalid JSON');
+
+const output = objectParser(inpStr)[0];
+
+console.log(JSON.stringify(output, null, 2));
